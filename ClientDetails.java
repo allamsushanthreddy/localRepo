a39c165eaa2d2b23b0e2641f13a228e3b42f253c
allamsushanthreddy <100283131+allamsushanthreddy@users.noreply.github.com>
2024-10-25 12:56:50 +0530

    new repo

---
+++ ClientDetails.java
@@ -0,0 +1,23 @@
+class ClientDetails {
+
+	public static void main(String[] args) {
+
+		// 1. Declare variables
+		String welcome;
+		String name;
+		String emailAddress;
+		Long phoneNumber;
+
+		// 2. Initialize or assign values to variables/ data members
+		welcome = "Welcome to GLCA Backend Development Training";
+		name = "Joseph Pinto";
+		emailAddress = " joseph@gmail.com";
+		phoneNumber = 9898989898898989L;
+
+		// 3. Print the values
+		System.out.println(welcome);
+		System.out.println("Name: "+name);
+		System.out.println("Email: "+emailAddress);
+		System.out.println("Phone Number: "+phoneNumber);
+	}
+}
